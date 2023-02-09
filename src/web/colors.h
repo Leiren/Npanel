8c64c114e094b3a3af9622c8536cee89ea8fb9da
Leiren <nomail>
2023-02-09 05:43:46 +0330

    static linking runtime lib to fix dll misisngs

--- src/web/colors.h
+++ src/web/colors.h
@@ -14,8 +14,7 @@ public:
     static constexpr ImVec4 orange = ImVec4(1.000f, 0.600f, 0.000f, 0.65f);
     static constexpr ImVec4 softblack =  ImVec4(0.118f, 0.118f, 0.118f, 1.000f);
     static constexpr ImVec4 red =  ImVec4(1.000f, 0.201f, 0.089f, 1.000f);
-
-    
+   
     static constexpr ImVec4 transparent = ImVec4(0.0f, 0.0f, 0.0f, 0.0f);
 };
 
